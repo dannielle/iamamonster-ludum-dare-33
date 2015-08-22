05af919e15752f577ad3cd770ae2be6b08bb7672
dannielle <dannielle.del@gmail.com>
2015-08-22 16:59:56 -0400

    stabbing and win condition prints in console

---
+++ Assets/StabbingScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StabbingScript : MonoBehaviour {
+
+	int stabCount;
+
+	void Start () {
+		stabCount = 0;
+	}
+	
+	void Update () {
+		if (stabCount >= 10) {
+			GameObject gm = GameObject.Find ("GameManager");
+			((GameScript) gm.GetComponent(typeof(GameScript))).win();
+		}
+	}
+
+	void OnTriggerStay2D(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			print("stab");
+			stabCount++;
+		}
+	}
+}
