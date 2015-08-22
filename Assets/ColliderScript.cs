8baf33f355b9fce167d011d1af73093286850bb9
dannielle <dannielle.del@gmail.com>
2015-08-22 15:51:46 -0400

    climbing and adventuring modes for player donenenne

---
+++ Assets/ColliderScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderScript : MonoBehaviour {
+
+	GameObject player;
+
+	void Start () {
+		player = GameObject.Find ("player");
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnCollisionStay2D(){
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			GetComponent<Collider2D>().isTrigger = true;
+			((PlayerScript)player.GetComponent(typeof(PlayerScript))).startsClimbing();
+		}
+	}
+
+	void OnTriggerExit2D(){
+		GetComponent<Collider2D>().isTrigger = false;
+		((PlayerScript)player.GetComponent(typeof(PlayerScript))).startsAdventuring();
+	}
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,17 +5,45 @@ public class PlayerScript : MonoBehaviour {
 
 	private Vector2 movement;
 
+	enum State {
+		Climbing,
+		Adventuring
+	}
+
+	State state;
+
 	void Start () {
-	
+		state = State.Adventuring;
 	}
 	
 	void Update () {
 		float inputX = Input.GetAxis ("Horizontal");
 		float inputY = Input.GetAxis ("Vertical");
-		movement = new Vector2(inputX, inputY);
+		movement = new Vector2(2* inputX, 2* inputY);
+
+		switch (state) {
+		case State.Climbing:
+			GetComponent<Rigidbody2D>().gravityScale = 0;
+
+			break;
+		case State.Adventuring:
+			GetComponent<Rigidbody2D>().gravityScale = 15;
+
+			break;
+		default:
+			break;
+		}
 	}
 
 	void FixedUpdate () {
 		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+
+	public void startsClimbing() {
+		state = State.Climbing;
+	}
+
+	public void startsAdventuring() {
+		state = State.Adventuring;
 	}
 }
