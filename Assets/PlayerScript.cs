18c4909ecf574449cb4fec07a0540bd75671332a
dannielle <dannielle.del@gmail.com>
2015-08-22 15:28:15 -0400

    player movement

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Vector2 movement;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		movement = new Vector2(inputX, inputY);
+	}
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
